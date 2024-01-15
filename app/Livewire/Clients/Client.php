ebc686c863d43fba09aedb132cce14d1ed971271
Chetan Singh <chetan.singh@webeesocial.com>
2024-01-15 09:57:04 +0530

    added logic for client vierw

--- app/Livewire/Clients/Client.php
+++ app/Livewire/Clients/Client.php
@@ -3,11 +3,24 @@
 namespace App\Livewire\Clients;
 
 use Livewire\Component;
+use App\Models\Client as ClientModel;
+use Illuminate\Http\Request;
 
 class Client extends Component
 {
+
+    public $client_id;
+
+    public $client;
+
     public function render()
     {
         return view('livewire.clients.client');
+    }
+
+    public function boot(Request $request)
+    {
+        $this->client_id =  $request->id;
+        $this->client = ClientModel::find($this->client_id);
     }
 }
