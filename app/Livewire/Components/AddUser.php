4060b99713629c3f44cc3d2936479096c82c45aa
Chetan Singh <chetan.singh@webeesocial.com>
2024-03-21 11:06:16 +0530

    connected users list page and user inner page filter is left

--- app/Livewire/Components/AddUser.php
+++ app/Livewire/Components/AddUser.php
@@ -7,15 +7,24 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Team;
 use App\Helpers\Helper;
+use Livewire\WithFileUploads;
 
 class AddUser extends Component
 {
+    use WithFileUploads;
+
+    protected $listeners = ['addUser','editUser'];
+
     public $name;
     public $email;
     public $password;
     public $designation;
     public $teams = [];
     public $selectedTeams = [];
+
+    // for edit
+
+    public $user;
 
 
     public function render()
@@ -29,6 +38,11 @@ class AddUser extends Component
     }
 
     public function addUser(){
+        if($this->user){
+            $this->updateUser();
+            return;
+        }
+
         $this->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users,email',
@@ -47,5 +61,31 @@ class AddUser extends Component
         $user->teams()->sync($this->selectedTeams);
         $this->dispatch('saved');
         $this->dispatch('user-added');
+    }
+
+    public function editUser($user_id){
+        $user = User::find($user_id);
+        $this->user = $user;
+        $this->name = $user->name;
+        $this->email = $user->email;
+        $this->designation = $user->designation;
+        $this->selectedTeams = $user->teams->pluck('id')->toArray();
+        $this->dispatch('edit-user',$this->selectedTeams);
+    }
+
+    public function updateUser(){
+        $this->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,'.$this->user->id,
+        ]);
+
+        $user = $this->user;
+        $user->name = $this->name;
+        $user->email = $this->email;
+        $user->designation = $this->designation;
+        $user->save();
+        $user->teams()->sync($this->selectedTeams);
+        $this->dispatch('saved');
+        $this->dispatch('user-updated');
     }
 }
