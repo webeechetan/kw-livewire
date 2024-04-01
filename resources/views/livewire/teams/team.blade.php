602630ce7b61ace442a450e6494e1ed079899cb0
hsharma123 <bhs985576@gmail.com>
2024-04-01 19:14:56 +0530

    Roles and permission Desgin

--- resources/views/livewire/teams/team.blade.php
+++ resources/views/livewire/teams/team.blade.php
@@ -1,3 +1,38 @@
-<div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+<div class="container">
+    <!-- Dashboard Header -->
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a wire:navigate href="{{ route('dashboard') }}"><i class='bx bx-line-chart'></i> Dashboard</a></li>
+            <li class="breadcrumb-item"><a wire:navigate href="{{ route('team.index') }}">All Team</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Tech Team</li>
+        </ol>
+    </nav>
+    <div class="row">
+    <div class="col-lg-4">
+            <div class="column-box">
+                <div class="user-profile">
+                    <div class="user-profile-img"><img src="/storage/images/user_pro.jpg" alt=""></div>
+                    <h3 class="main-body-header-title mb-2">Tech Team</h3>
+                   
+                    <div class="row">
+                        <div class="col-lg-8 mx-auto">
+                            <div class="row align-items-center justify-content-center gap-2">
+                              
+                                <div class="col-auto">
+                                    <div class="card_style-user-head-position"><i class="bx bx-user text-primary"></i> Web Developer</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="row align-items-center mb-2">
+                </div>
+                <div class="row align-items-center mb-2">
+                </div>
+                <hr>
+                                     
+            </div>
+        </div>
+    </div>
 </div>
