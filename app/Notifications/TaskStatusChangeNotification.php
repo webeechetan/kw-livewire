466ea08d188f5e4d7135a9d29d8703f7feab8c4d
Chetan Singh <chetan.singh@webeesocial.com>
2024-08-26 19:13:47 +0530

    fixed task status text in mails

--- app/Notifications/TaskStatusChangeNotification.php
+++ app/Notifications/TaskStatusChangeNotification.php
@@ -42,6 +42,31 @@ class TaskStatusChangeNotification extends Notification implements ShouldQueue
      */
     public function toMail(object $notifiable): MailMessage
     {
+        if($this->oldStatus == 'completed'){
+            $this->oldStatus = 'Completed';
+        }else if($this->oldStatus == 'in_progress'){
+            $this->oldStatus = 'In Progress';
+        }else if($this->oldStatus == 'in_review'){
+            $this->oldStatus = 'In Review';
+        }else if($this->oldStatus == 'pending'){
+            $this->oldStatus = 'Pending';
+        }else{
+            $this->oldStatus = 'Not Started';
+        }
+
+        if($this->newStatus == 'completed'){
+            $this->newStatus = 'Completed';
+        }else if($this->newStatus == 'in_progress'){
+            $this->newStatus = 'In Progress';
+        }else if($this->newStatus == 'in_review'){
+            $this->newStatus = 'In Review';
+        }else if($this->newStatus == 'pending'){
+            $this->newStatus = 'Pending';
+        }else{
+            $this->newStatus = 'Not Started';
+        }
+
+
 
         return (new MailMessage)
             ->view('mails.task-status-change', ['task' => $this->task, 'user' => $notifiable, 'oldStatus' => $this->oldStatus, 'newStatus' => $this->newStatus, 'changedBy' => $this->changedBy]);
