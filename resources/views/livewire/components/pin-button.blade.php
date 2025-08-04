72003d0ca3fe30084633e46fbf7fedbdd7cadb34
roshanwebee <roshanjajoriya1994@gmail.com>
2025-08-04 18:55:37 +0530

    Worked on new user admin screen

--- resources/views/livewire/components/pin-button.blade.php
+++ resources/views/livewire/components/pin-button.blade.php
@@ -1,16 +1,17 @@
 <div>
-    <a href="javascript:" class="btn-sm btn-border btn-border-primary" wire:click="togglePin"
-        wire:loading.attr="disabled">
+    <button type="button" class="btn-sm btn-border btn-border-primary @if($pinned) active @endif" wire:click="togglePin" wire:loading.attr="disabled">
         
         <span wire:loading wire:target="togglePin">
             <i class='bx bx-loader-alt bx-spin'></i>
         </span>
     
         <i class='bx bx-pin'></i>
-        @if($pinned)
-            Unpin
-        @else
-            Pin
+        @if($content)
+            @if($pinned)
+                Unpin
+            @else
+                Pin
+            @endif
         @endif
-    </a>
+    </button>
 </div>
